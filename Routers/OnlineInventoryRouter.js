2b25c040654e03a75a58de3cf3a875f2d4fc518e
Vignesh3161 <vickysvignesh3161@gmail.com>
2026-08-10 18:43:05 +0530

    update online inventory

--- Routers/OnlineInventoryRouter.js
+++ Routers/OnlineInventoryRouter.js
@@ -13,7 +13,7 @@ import { authenticate, authorize } from '../Middlewares/index.js';
 const router = express.Router();
 
 router.use(authenticate);
-
+// 
 // 1. Adjust Online Stock (Place before dynamic ID routes)
 router.put('/adjust', authorize(['ADMIN', 'STOCK_MANAGER']), adjustOnlineStock);
 
